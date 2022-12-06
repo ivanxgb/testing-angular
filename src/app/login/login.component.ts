99a8d67a9f854793ef497836c3031ac8c52a73c9
ivanxgb <ivanxgb@gmail.com>
2022-12-05 22:28:54 -0500

    Implemented form for login, added basic styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators as V} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  registerForm!: FormGroup
+  showRegisterForm: boolean = false;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [V.required, V.email]],
+      password: ['', [V.required, V.minLength(6)]]
+    });
+
+    this.registerForm = this.fb.group({
+      email: ['', [V.required, V.email]],
+      password: ['', V.required],
+      confirmPassword: ['', [V.required, V.minLength(6)]]
+    });
+  }
+
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+  toggleRegister() {
+    this.showRegisterForm = !this.showRegisterForm;
+  }
 }
