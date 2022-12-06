15f4d04d03d70eec41d7e966484d5e0d099533d7
ivanxgb <ivanxgb@gmail.com>
2022-12-05 22:02:09 -0500

    Added methods login, register and interface for response

---
+++ src/app/response-interfaces/authInterface.ts
@@ -0,0 +1,5 @@
+export interface AuthInterface {
+  id: number | undefined;
+  token: string | undefined;
+  error: string | undefined;
+}
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/env";
+import {HttpClient} from "@angular/common/http";
+import {AuthInterface} from "../../response-interfaces/authInterface";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private host = environment.urlApi;
+  constructor(private httpClient: HttpClient) { }
+
+  login(email: string | null = null, password: string | null = null): Observable<AuthInterface> {
+    return this.httpClient.post<AuthInterface>(`${this.host}login`, {
+      email,
+      password
+    });
+  }
+
+  register(email: string | null = null, password: string | null = null): Observable<AuthInterface> {
+    return this.httpClient.post<AuthInterface>(`${this.host}register`, {
+      email,
+      password
+    });
+  }
 }
