3253d1210f2acb948406e5ba9f585fc24d7e36a5
ivanxgb <ivanxgb@gmail.com>
2022-12-06 22:26:05 -0500

    Implemented guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginGuard} from "./guard/login.guard";
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    canActivate: [LoginGuard]
+  }
 
 ];
 
