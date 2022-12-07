36cccd382309ee31069e60717df070ef47e3bb85
ivanxgb <ivanxgb@gmail.com>
2022-12-06 22:36:07 -0500

    Implemented button for login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isAuthenticated: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  onLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.isAuthenticated = false;
+  }
 }
