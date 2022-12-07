906e94302b581cdbc5b2687587f69b56ed4791c7
ivanxgb <ivanxgb@gmail.com>
2022-12-06 23:27:47 -0500

    Added test for login.guard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,4 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("Testing");
-  });
-
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain(
-      "Testing app is running!"
-    );
-  });
 });
--- src/app/guard/login.guard.spec.ts
+++ src/app/guard/login.guard.spec.ts
@@ -1,16 +1,57 @@
 import { TestBed } from "@angular/core/testing";
 
 import { LoginGuard } from "./login.guard";
+import { AuthService } from "../services/auth/auth.service";
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("LoginGuard", () => {
   let guard: LoginGuard;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [AuthService],
+    });
     guard = TestBed.inject(LoginGuard);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
   });
 
   it("should be created", () => {
     expect(guard).toBeTruthy();
+  });
+
+  it("should allow access if the user is not authenticated", () => {
+    spyOn(authService, "isAuthenticated").and.returnValue(false);
+    const spy = spyOn(router, "navigate");
+    const activatedRouteSnapshot: ActivatedRouteSnapshot =
+      {} as ActivatedRouteSnapshot;
+    const routerStateSnapshot: RouterStateSnapshot = {} as RouterStateSnapshot;
+
+    expect(
+      guard.canActivate(activatedRouteSnapshot, routerStateSnapshot)
+    ).toBeTruthy();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it("should not allow access if the user is authenticated", () => {
+    spyOn(authService, "isAuthenticated").and.returnValue(true);
+    const spy = spyOn(router, "navigate");
+    const activatedRouteSnapshot: ActivatedRouteSnapshot =
+      {} as ActivatedRouteSnapshot;
+    const routerStateSnapshot: RouterStateSnapshot = {} as RouterStateSnapshot;
+
+    expect(
+      guard.canActivate(activatedRouteSnapshot, routerStateSnapshot)
+    ).toBeFalsy();
+    expect(spy).toHaveBeenCalledWith(["/"]);
   });
 });
