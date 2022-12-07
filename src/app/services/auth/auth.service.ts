96a849a51466e4b310b9020d0c20ca376cbc459b
ivanxgb <ivanxgb@gmail.com>
2022-12-06 22:27:18 -0500

    Implemented login, added method to check if is authenticated, pipe for save in local storage

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/env";
 import {HttpClient} from "@angular/common/http";
 import {AuthInterface} from "../../response-interfaces/authInterface";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 import {AuthBodyInterface} from "../../response-interfaces/AuthBody.interface";
 
 @Injectable({
@@ -13,11 +13,41 @@ export class AuthService {
   private host = environment.urlApi;
   constructor(private httpClient: HttpClient) { }
 
-  login(body: AuthBodyInterface): Observable<AuthInterface> {
-    return this.httpClient.post<AuthInterface>(`${this.host}login`, body);
+  login(body: AuthBodyInterface): Observable<Boolean> {
+    return this.httpClient.post<AuthInterface>(`${this.host}login`, body).pipe(
+      map((response: AuthInterface) => {
+        if (response.token) {
+          this.handleSave(response);
+        }
+        return !!response.token;
+      },
+    ));
   }
 
-  register(body: AuthBodyInterface): Observable<AuthInterface> {
-    return this.httpClient.post<AuthInterface>(`${this.host}register`, body);
+  register(body: AuthBodyInterface): Observable<Boolean> {
+    return this.httpClient.post<AuthInterface>(`${this.host}register`, body).pipe(
+      map((response: AuthInterface) => {
+        if (response.token) {
+          this.handleSave(response);
+        }
+        return !!response.token;
+      },
+    ));
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  handleSave(response: AuthInterface) {
+    this.saveToken(response.token!);
   }
 }
